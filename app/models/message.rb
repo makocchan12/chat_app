64b00814ec463526f39f231c68899be5827b6fed
makocchan12 <shootingstar1119@gmail.com>
2020-10-01 12:57:25 +0900

    各モデルのテスト

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,7 @@
 class Message < ApplicationRecord
-  belongs_to : room
-  belongs_to : user
-  has_one_attached : image
+  belongs_to :room
+  belongs_to :user
+  has_one_attached :image
 
   validates :content, presence: true, unless: :was_attached?
 
